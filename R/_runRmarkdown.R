d825e64f3ee57ae06d6e7c7de1c73c0b8420e35e
MartinBulla <bulla.mar@gmail.com>
2025-12-01 20:25:35 +0100

    updated SI and readme

--- R/_runRmarkdown.R
+++ R/_runRmarkdown.R
@@ -1,6 +1,6 @@
 require(rmarkdown)
 
-rmarkdown::render('R/rep_SI.R', output_dir = 'Output',  output_file = 'MS_test_6.html') #, quiet = TRUE)
+rmarkdown::render('R/rep_SI_v2.R', output_dir = 'Output', output_file = 'SI_v3.html') #, quiet = TRUE)
 
 rmarkdown::render('R/test.R', output_dir = 'Output',  output_file = 'test.html')#, quiet = TRUE)
 
